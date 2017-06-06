f36c527ffead68509e404ab6890c3458d4699028
FlorianMeyer-PC\FlorianMeyer <florian.meyer@edu.hefr.ch>
2017-06-06 23:22:11 +0200

    change background and add some picture

--- GOF/Assets/Home_Created/scripts/Levels/LevelChoiceButtons.cs
+++ GOF/Assets/Home_Created/scripts/Levels/LevelChoiceButtons.cs
@@ -3,20 +3,36 @@ using UnityEngine.UI;
 
 public class LevelChoiceButtons : MonoBehaviour {
 
+    public Sprite[] listSprite;
+    public Image mainImage;
+
 	private LevelManagement levelManagement;
+    private int countClick;
 
 	void Start()
 	{
+        countClick = 0;
+        if (listSprite[countClick] != null) mainImage.sprite = listSprite[countClick];
 		levelManagement = FindObjectOfType<LevelManagement> ();
 	}
 
 	public void btnNextClicked()
 	{
+        if(countClick < (listSprite.Length-1))
+        {
+            countClick++;
+            mainImage.sprite = listSprite[countClick];
+        }
 		levelManagement.next ();
 	}
 
 	public void btnPreviousClicked()
 	{
-		levelManagement.previous ();
+        if (countClick > 0)
+        {
+            countClick--;
+            mainImage.sprite = listSprite[countClick];
+        }
+        levelManagement.previous ();
 	}
 }
